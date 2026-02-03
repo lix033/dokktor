8d5483ed04e49789cbd7627319e5a8500f78ac9b
Julien KOLOU <julien.kolou.ext@togocom.tg>
2026-02-03 16:04:17 +0000

    ci: build and push Docker images on master

--- frontend/old/src/components/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Point d'export centralisé pour tous les composants
- */
-export { ContainerCard } from './ContainerCard';
-export { ContainerList } from './ContainerList';
-export { LogsViewer } from './LogsViewer';
